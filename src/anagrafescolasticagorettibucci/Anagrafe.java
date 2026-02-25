82460acc0c76cce2806ee81b63ecc8720f85a84a
buccialex <bucci.alex@franchettisalviani.net>
2026-02-25 09:15:02 +0100

    Initial commit

---
+++ src/anagrafescolasticagorettibucci/Anagrafe.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package anagrafescolasticagorettibucci;
+
+import java.util.*;
+
+/**
+ *
+ * @author goretti.leonardo
+ */
+public class Anagrafe {
+   private ArrayList <Studente> listaStudenti;
+   private HashSet <String> insiemeMatricole;
+   private HashMap <String, Studente> mappaStudenti;
+   
+}
---
+++ src/anagrafescolasticagorettibucci/Studente.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package anagrafescolasticagorettibucci;
+
+/**
+ *
+ * @author goretti.leonardo
+ */
+public class Studente {
+    String nome, cognome, matricola;
+
+    public Studente(String nome, String cognome, String matricola) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.matricola = matricola;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(String matricola) {
+        this.matricola = matricola;
+    }
+    
+    
+}
